490055cbd9c63b9019b19dc0ab3ce5bb9280cf50
Rafael N Santos <42522990+rnds1@users.noreply.github.com>
2020-04-26 02:12:47 -0300

    Create Local.cs

---
+++ Local.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary id="Local">
+/// Locais onde cartas se agrupam
+/// <c>Mão do jogador</c><c>Deck</c> <c> Cimiterio</c>
+/// </summary>
+
+public class Local : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<GameObject> Cartas;
+    public Vector3 DirecaoQ_a_CartaVai;
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        
+    }
+}
